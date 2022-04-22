7228e959301b308854395e6c5ff14aced1e476cd
Aanchi-glitch2744 <aanchalkumari27444@gmail.com>
2022-04-22 09:55:12 +0530

    Updated packages

---
+++ build/precache-manifest.0e82532dc167e9be3a6da4d2cc57569b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33eeaf3155aa596340367a60ec97f0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "19a9a1d158f68e68b430",
+    "url": "/static/css/main.956d9460.chunk.css"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/static/js/2.6cdbf0dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6cdbf0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a9a1d158f68e68b430",
+    "url": "/static/js/main.9c7e7031.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  }
+]);
